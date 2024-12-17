6ac66545df1676472e085a8f387b13227fc72209
HelloHailie <alsltjsals@gmail.com>
2024-12-18 00:18:01 +0900

    Fix: 사용하지 않는 파일 제거

--- examples/main.tsx
+++ examples/main.tsx
@@ -2,13 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import AnimatedApp from "./animated/App";
 import CustomSettingApp from "./custom-settings/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <AnimatedApp />
     <CustomSettingApp />
   </StrictMode>
 );
