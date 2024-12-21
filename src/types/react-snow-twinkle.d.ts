560ef27a513a9e926f958d71260c5e2f8dccb06a
HelloHailie <alsltjsals@gmail.com>
2024-12-21 14:57:50 +0900

    Feat: 리액트 스노우 트윙클 타입 선언 파일 추가
    - TypeScript 지원을 위한 .d.ts 파일 생성
    - 컴포넌트 props 인터페이스 정의 (snowflakeCount, fallSpeed 등)
    - JSDoc 주석을 통한 props 설명 및 기본값 문서화

---
+++ src/types/react-snow-twinkle.d.ts
@@ -0,0 +1,36 @@
+declare module "react-snow-twinkle" {
+  export interface SnowTwinkleProps {
+    /**
+     * 눈송이의 개수
+     * @default 150
+     */
+    snowflakeCount?: number;
+
+    /**
+     * 눈송이가 떨어지는 속도
+     * @default 1
+     */
+    fallSpeed?: number;
+
+    /**
+     * 눈송이의 크기
+     * @default 3
+     */
+    flakeSize?: number | "mix";
+
+    /**
+     * 눈송이의 투명도
+     * @default 1
+     */
+    opacity?: number;
+
+    /**
+     * 눈송이의 모양
+     * @default "❆"
+     */
+    flakeShape?: string;
+  }
+
+  const SnowTwinkle: React.FC<SnowTwinkleProps>;
+  export default SnowTwinkle;
+}
